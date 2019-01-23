8c45db2d9cd734e68278519a55074d4cc47c8ea5
JuniorBelinda <belinda.schuehle@nyp.ch>
2019-01-23 15:15:39 +0100

    Auto stash before merge of "develop" and "origin/develop"

--- src/main/java/ch/noseryoung/lea/CourseController.java
+++ src/main/java/ch/noseryoung/lea/CourseController.java
@@ -4,8 +4,11 @@ import java.util.Arrays;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,6 +22,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/courses")
 public class CourseController {
+	
+	/**
+	 * This method creates a new course
+	 * @return ResponseEntity with the course that was created
+	 */
+	@PostMapping("/post/{id}")
+	public @ResponseBody ResponseEntity<Course> postById(@PathVariable long id){
+		Course course = new Course(new Long(id), "module222");
+		return new ResponseEntity<>(course, HttpStatus.OK);
+	}
 	
 	/**
 	 * This method returns the requested course
@@ -43,4 +56,26 @@ public class CourseController {
 		Course course2 = new Course(new Long(2),"module223");
 		return new ResponseEntity<>(Arrays.asList(course1,course2), HttpStatus.OK);
 	}
+	
+	/**
+	 * This method updated a course
+	 * @return ResponseEntity with the course that was updated 
+	 */
+	@PutMapping("/put/{id}")
+	public @ResponseBody ResponseEntity<Course> putById(@PathVariable long id){
+	Course course = new Course(new Long(id), "module224");
+	return new ResponseEntity<>(course, HttpStatus.OK);
+	}	
+	
+	/**
+	 * This method deletes a course
+	 * @return ResponseEntity with the course that was deleted
+	 */
+	@DeleteMapping("/delete/{id}")
+	public @ResponseBody ResponseEntity<Course> deleteById(@PathVariable long id){
+		Course course = new Course(new Long(id), "module225");
+		return new ResponseEntity<>(course, HttpStatus.OK);
+	
+	}
+	
 }
