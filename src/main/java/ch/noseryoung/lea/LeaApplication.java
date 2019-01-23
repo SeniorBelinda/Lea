7eef63e52f2f2f1f21f13118f5887b403701640e
SeniorBelinda <46001325+SeniorBelinda@users.noreply.github.com>
2019-01-23 12:31:36 +0100

    Initial Upload

---
+++ src/main/java/ch/noseryoung/lea/LeaApplication.java
@@ -0,0 +1,14 @@
+package ch.noseryoung.lea;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeaApplication.class, args);
+	}
+
+}
+
